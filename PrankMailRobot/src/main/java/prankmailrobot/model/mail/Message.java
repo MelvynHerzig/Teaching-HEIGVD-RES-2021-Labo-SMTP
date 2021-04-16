0930efd6fb717d6c2e33998f38102ee54e136d11
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-04-16 13:44:19 +0200

    Modélasion mail

--- PrankMailRobot/src/main/java/prankmailrobot/model/mail/Message.java
+++ PrankMailRobot/src/main/java/prankmailrobot/model/mail/Message.java
@@ -21,16 +21,87 @@ public class Message
    /**
     * Expéditeur
     */
-   private String from;
+   private Person from;
 
    /**
     * Liste des récepteurs.
     */
-   private final ArrayList<String> to = new ArrayList<>();
+   private ArrayList<Person> to;
 
    /**
     * Liste des personnes jointes.
     */
-   private final ArrayList<String> cc = new ArrayList<>();
+   private ArrayList<Person> cc;
 
+   /**
+    * Sujet du mail
+    */
+   private String subject;
+
+   /**
+    * Contenu du mail
+    */
+   private String content;
+
+   /**
+    * Constructeur.
+    * @param from Expéditeur
+    * @param to Récepteurs
+    * @param cc En copies.
+    * @param subject Sujet
+    * @param content Contenu du mail.
+    */
+   public Message(Person from, ArrayList<Person> to, ArrayList<Person> cc, String subject, String content)
+   {
+      this.from = from;
+      this.to = to;
+      this.cc = cc;
+      this.subject = subject;
+      this.content = content;
+   }
+
+   /**
+    * Getter expéditeur
+    * @return Retourne l'expéditeur.
+    */
+   public Person getFrom()
+   {
+      return from;
+   }
+
+   /**
+    * Getter des récepteurs
+    * @return Retourne les récepteurs.
+    */
+   public ArrayList<Person> getTo()
+   {
+      return to;
+   }
+
+   /**
+    * Getter des personnes en copies.
+    * @return Retourne les personnes en copies.
+    */
+   public ArrayList<Person> getCc()
+   {
+      return cc;
+   }
+
+   /**
+    * Getter du sujet du mail.
+    * @return Retourne le sujet du mail.
+    */
+   public String getSubject()
+   {
+      return subject;
+   }
+
+   /**
+    * Getter du contenu du mail.
+    * @return Retourne le contenu du mail.
+    */
+   public String getContent()
+   {
+      return content;
+   }
 }
