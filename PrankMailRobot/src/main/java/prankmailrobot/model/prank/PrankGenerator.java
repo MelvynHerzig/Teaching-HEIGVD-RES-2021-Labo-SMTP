ae46bafd3e297a25f3f9d2d90d75f32a2b1860cb
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-04-16 14:16:48 +0200

    Interface smtp Client

--- PrankMailRobot/src/main/java/prankmailrobot/model/prank/PrankGenerator.java
+++ PrankMailRobot/src/main/java/prankmailrobot/model/prank/PrankGenerator.java
@@ -1,5 +1,19 @@
+/*
+ -----------------------------------------------------------------------------------
+ Cours       : RES
+ Fichier     : prankmailrobot.model.prank.PrankGenerator.java
+ Auteur(s)   : Forestier Quentin & Herzig Melvyn
+ Date        : 16.04.2021
+ -----------------------------------------------------------------------------------
+ */
+
 package prankmailrobot.model.prank;
 
+/**
+ * Classe permettant de générer des pranks.
+ * @author Forestier Quentin et Herzig Melvyn
+ * @date 16/04/2021
+ */
 public class PrankGenerator
 {
 }
--- PrankMailRobot/src/main/java/prankmailrobot/smtp/ISmtpClient.java
+++ PrankMailRobot/src/main/java/prankmailrobot/smtp/ISmtpClient.java
@@ -1,5 +1,26 @@
+/*
+ -----------------------------------------------------------------------------------
+ Cours       : RES
+ Fichier     : prankmailrobot.smtp.ISmtpClient.java
+ Auteur(s)   : Forestier Quentin & Herzig Melvyn
+ Date        : 16.04.2021
+ -----------------------------------------------------------------------------------
+ */
+
 package prankmailrobot.smtp;
 
+import prankmailrobot.model.mail.Message;
+
+/**
+ * Interface a implémenter pour être un smtp client du prankMailRobot.
+ * @author Forestier Quentin et Herzig Melvyn
+ * @date 16/04/2021
+ */
 public interface ISmtpClient
 {
+   /**
+    * Envoie les messages en paramètre grâce au protocole SMTP.
+    * @param messages
+    */
+   public void sendMessages(Message ... messages);
 }
